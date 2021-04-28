5c47341fc24be771d949a82312b9f620339c25dc
rosabandpey <mahnazbandpey@gmail.com>
2021-04-28 19:35:17 +0430

    Implementing findPlace method for Place class

--- src/main/java/com/camp/campApi/controller/PlaceController.java
+++ src/main/java/com/camp/campApi/controller/PlaceController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,8 +33,8 @@ public class PlaceController {
 
     }
 
-    @GetMapping("/findPlace")
-    public ResponseEntity<?> findPlace(long id)  {
+    @GetMapping("/findPlace/{id}")
+    public ResponseEntity<?> findPlace(@PathVariable("id") long id)  {
 
         Place place=placeService.findPlaceById(id);
 
