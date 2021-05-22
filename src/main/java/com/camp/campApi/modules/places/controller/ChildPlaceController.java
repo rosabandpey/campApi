ed0725776a1dd08ee63c8cce9462b5978a8115e8
rosabandpey <mahnazbandpey@gmail.com>
2021-05-22 19:44:33 +0430

    Customizing object of childPlace for api

--- src/main/java/com/camp/campApi/modules/places/controller/ChildPlaceController.java
+++ src/main/java/com/camp/campApi/modules/places/controller/ChildPlaceController.java
@@ -2,7 +2,12 @@ package com.camp.campApi.modules.places.controller;
 
 import com.camp.campApi.modules.places.entity.ChildPlace;
 import com.camp.campApi.entity.ResponseApi;
+import com.camp.campApi.modules.places.entity.ChildPlaceResponse;
+import com.camp.campApi.modules.places.entity.Place;
 import com.camp.campApi.modules.places.service.ChildPlaceService;
+import com.camp.campApi.modules.places.service.PlaceService;
+import com.camp.campApi.modules.users.entity.AppUser;
+import com.camp.campApi.modules.users.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,6 +25,13 @@ public class ChildPlaceController {
 
 
     ChildPlaceService childPlaceService;
+    PlaceService placeService;
+    UserService userService;
+
+    public ChildPlaceController(ChildPlaceService childPlaceService, PlaceService placeService) {
+        this.childPlaceService = childPlaceService;
+        this.placeService = placeService;
+    }
 
     @Autowired
     public ChildPlaceController(ChildPlaceService childPlaceService) {
@@ -27,9 +39,36 @@ public class ChildPlaceController {
     }
 
     @RequestMapping(value = "/savePlace",produces ={ MediaType.APPLICATION_JSON_VALUE},method = {RequestMethod.GET,RequestMethod.POST} )
-    public ResponseEntity<?> savePlace(@RequestBody ChildPlace childPlace)  {
-        ChildPlace place=childPlaceService.registerChildPlace(childPlace);
-        ResponseApi responseApi=new ResponseApi(true,HttpStatus.OK.toString(),new Date().toString(), Arrays.asList(place) );
+    public ResponseEntity<?> savePlace(@RequestBody ChildPlaceResponse childPlaceResponse)  {
+
+        Place place=placeService.findPlaceById(childPlaceResponse.getMychildplace());
+        AppUser appUser=userService.findByUserId(childPlaceResponse.getUserChildPlace());
+        ChildPlace childPlace=new ChildPlace();
+        childPlace.setMychildplace(place);
+        childPlace.setUserChildPlace(appUser);
+        childPlace.setChildADAaccessible(childPlaceResponse.getChildADAaccessible());
+        childPlace.setChildCity(childPlaceResponse.getChildCity());
+        childPlace.setChildAddress(childPlaceResponse.getChildAddress());
+        childPlace.setChildCleanness(childPlaceResponse.getChildCleanness());
+        childPlace.setChildCountry(childPlaceResponse.getChildCountry());
+        childPlace.setChildCellSignal(childPlaceResponse.getChildCellSignal());
+        childPlace.setChildCrowdness(childPlaceResponse.getChildCrowdness());
+        childPlace.setChildWifi(childPlaceResponse.getChildWifi());
+        childPlace.setChildWater(childPlaceResponse.getChildWater());
+        childPlace.setChildTrash(childPlaceResponse.getChildTrash());
+        childPlace.setChildFirePits(childPlaceResponse.getChildFirePits());
+        childPlace.setChildManNoise(childPlaceResponse.getChildManNoise());
+        childPlace.setChildElectronic(childPlaceResponse.getChildElectronic());
+        childPlace.setChildName(childPlaceResponse.getChildName());
+        childPlace.setChildLastVisit(childPlaceResponse.getChildLastVisit());
+        childPlace.setChildPetAllowed(childPlaceResponse.getChildPetAllowed());
+        childPlace.setChildSafty(childPlaceResponse.getChildSafty());
+        childPlace.setChildLocation(childPlaceResponse.getChildLocation());
+        childPlace.setChildPicnicTable(childPlaceResponse.getChildPicnicTable());
+        childPlace.setChildShower(childPlaceResponse.getChildShower());
+
+        childPlace=childPlaceService.registerChildPlace(childPlace);
+        ResponseApi responseApi=new ResponseApi(true,HttpStatus.OK.toString(),new Date().toString(), Arrays.asList(childPlace) );
         return new ResponseEntity<>(responseApi,HttpStatus.OK );
     }
 
---
+++ src/main/java/com/camp/campApi/modules/places/entity/ChildPlaceResponse.java
@@ -0,0 +1,287 @@
+package com.camp.campApi.modules.places.entity;
+
+import java.util.Date;
+
+public class ChildPlaceResponse {
+
+    private long id;
+
+    private long mychildplace;
+
+    private long userChildPlace;
+
+    private String childName;
+
+    private String childLocation;
+
+    private String childDetails;
+
+    private String childCountry;
+
+    private String childCity;
+
+    private String childAddress;
+
+    private int childCellSignal;
+
+    private Date childLastVisit;
+
+    private int childCrowdness;
+
+    private int childManNoise;
+
+    private int childCleanness;
+
+    private int childSafty;
+
+    private int childWater;
+
+    private int childElectronic;
+
+    private int childShower;
+
+    private int childTrash;
+
+    private int childPicnicTable;
+
+    private int childToilet;
+
+    private int childWifi;
+
+    private int childFirePits;
+
+    private int childPetAllowed;
+
+    private int childADAaccessible;
+
+    public ChildPlaceResponse() {
+    }
+
+    public ChildPlaceResponse(long id, long mychildplace, long userChildPlace, String childName, String childLocation, String childDetails, String childCountry, String childCity, String childAddress, int childCellSignal, Date childLastVisit, int childCrowdness, int childManNoise, int childCleanness, int childSafty, int childWater, int childElectronic, int childShower, int childTrash, int childPicnicTable, int childToilet, int childWifi, int childFirePits, int childPetAllowed, int childADAaccessible) {
+        this.id = id;
+        this.mychildplace = mychildplace;
+        this.userChildPlace = userChildPlace;
+        this.childName = childName;
+        this.childLocation = childLocation;
+        this.childDetails = childDetails;
+        this.childCountry = childCountry;
+        this.childCity = childCity;
+        this.childAddress = childAddress;
+        this.childCellSignal = childCellSignal;
+        this.childLastVisit = childLastVisit;
+        this.childCrowdness = childCrowdness;
+        this.childManNoise = childManNoise;
+        this.childCleanness = childCleanness;
+        this.childSafty = childSafty;
+        this.childWater = childWater;
+        this.childElectronic = childElectronic;
+        this.childShower = childShower;
+        this.childTrash = childTrash;
+        this.childPicnicTable = childPicnicTable;
+        this.childToilet = childToilet;
+        this.childWifi = childWifi;
+        this.childFirePits = childFirePits;
+        this.childPetAllowed = childPetAllowed;
+        this.childADAaccessible = childADAaccessible;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getMychildplace() {
+        return mychildplace;
+    }
+
+    public void setMychildplace(long mychildplace) {
+        this.mychildplace = mychildplace;
+    }
+
+    public long getUserChildPlace() {
+        return userChildPlace;
+    }
+
+    public void setUserChildPlace(long userChildPlace) {
+        this.userChildPlace = userChildPlace;
+    }
+
+    public String getChildName() {
+        return childName;
+    }
+
+    public void setChildName(String childName) {
+        this.childName = childName;
+    }
+
+    public String getChildLocation() {
+        return childLocation;
+    }
+
+    public void setChildLocation(String childLocation) {
+        this.childLocation = childLocation;
+    }
+
+    public String getChildDetails() {
+        return childDetails;
+    }
+
+    public void setChildDetails(String childDetails) {
+        this.childDetails = childDetails;
+    }
+
+    public String getChildCountry() {
+        return childCountry;
+    }
+
+    public void setChildCountry(String childCountry) {
+        this.childCountry = childCountry;
+    }
+
+    public String getChildCity() {
+        return childCity;
+    }
+
+    public void setChildCity(String childCity) {
+        this.childCity = childCity;
+    }
+
+    public String getChildAddress() {
+        return childAddress;
+    }
+
+    public void setChildAddress(String childAddress) {
+        this.childAddress = childAddress;
+    }
+
+    public int getChildCellSignal() {
+        return childCellSignal;
+    }
+
+    public void setChildCellSignal(int childCellSignal) {
+        this.childCellSignal = childCellSignal;
+    }
+
+    public Date getChildLastVisit() {
+        return childLastVisit;
+    }
+
+    public void setChildLastVisit(Date childLastVisit) {
+        this.childLastVisit = childLastVisit;
+    }
+
+    public int getChildCrowdness() {
+        return childCrowdness;
+    }
+
+    public void setChildCrowdness(int childCrowdness) {
+        this.childCrowdness = childCrowdness;
+    }
+
+    public int getChildManNoise() {
+        return childManNoise;
+    }
+
+    public void setChildManNoise(int childManNoise) {
+        this.childManNoise = childManNoise;
+    }
+
+    public int getChildCleanness() {
+        return childCleanness;
+    }
+
+    public void setChildCleanness(int childCleanness) {
+        this.childCleanness = childCleanness;
+    }
+
+    public int getChildSafty() {
+        return childSafty;
+    }
+
+    public void setChildSafty(int childSafty) {
+        this.childSafty = childSafty;
+    }
+
+    public int getChildWater() {
+        return childWater;
+    }
+
+    public void setChildWater(int childWater) {
+        this.childWater = childWater;
+    }
+
+    public int getChildElectronic() {
+        return childElectronic;
+    }
+
+    public void setChildElectronic(int childElectronic) {
+        this.childElectronic = childElectronic;
+    }
+
+    public int getChildShower() {
+        return childShower;
+    }
+
+    public void setChildShower(int childShower) {
+        this.childShower = childShower;
+    }
+
+    public int getChildTrash() {
+        return childTrash;
+    }
+
+    public void setChildTrash(int childTrash) {
+        this.childTrash = childTrash;
+    }
+
+    public int getChildPicnicTable() {
+        return childPicnicTable;
+    }
+
+    public void setChildPicnicTable(int childPicnicTable) {
+        this.childPicnicTable = childPicnicTable;
+    }
+
+    public int getChildToilet() {
+        return childToilet;
+    }
+
+    public void setChildToilet(int childToilet) {
+        this.childToilet = childToilet;
+    }
+
+    public int getChildWifi() {
+        return childWifi;
+    }
+
+    public void setChildWifi(int childWifi) {
+        this.childWifi = childWifi;
+    }
+
+    public int getChildFirePits() {
+        return childFirePits;
+    }
+
+    public void setChildFirePits(int childFirePits) {
+        this.childFirePits = childFirePits;
+    }
+
+    public int getChildPetAllowed() {
+        return childPetAllowed;
+    }
+
+    public void setChildPetAllowed(int childPetAllowed) {
+        this.childPetAllowed = childPetAllowed;
+    }
+
+    public int getChildADAaccessible() {
+        return childADAaccessible;
+    }
+
+    public void setChildADAaccessible(int childADAaccessible) {
+        this.childADAaccessible = childADAaccessible;
+    }
+}
