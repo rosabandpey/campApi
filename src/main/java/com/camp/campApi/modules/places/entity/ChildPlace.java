5dd72ef07653aa892165e14312f7dbc9e57a3e8d
rosabandpey <mahnazbandpey@gmail.com>
2021-05-24 14:19:01 +0430

    Customizing placeService method of findAll

--- src/main/java/com/camp/campApi/modules/places/entity/ChildPlace.java
+++ src/main/java/com/camp/campApi/modules/places/entity/ChildPlace.java
@@ -1,5 +1,7 @@
 package com.camp.campApi.modules.places.entity;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 
